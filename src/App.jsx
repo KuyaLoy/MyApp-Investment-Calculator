f57068cda783c8b8dcc389a6c08d28879bda449b
Robin Tapiru <robintapiru0894@gmail.com>
2023-11-20 18:04:36 +0400

    project

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import UserInput from "./components/UserInput";
+import Header from "./components/header";
+
+function App() {
+  return (
+    <>
+      <Header />
+      <UserInput />
+    </>
+  );
+}
+
+export default App;
